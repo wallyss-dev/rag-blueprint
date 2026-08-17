73ca4641ce266e32abfd1dced3dab9df3b00372a
Wallysson Uriel <wurielgames@gmail.com>
2026-08-17 16:40:11 -0300

    refactoring PDF extraction

--- meu-rag/src/pdf_reader.py
+++ meu-rag/src/pdf_reader.py
@@ -1,32 +1,36 @@
-from pathlib import Path 
+from pathlib import Path # Biblioteca orientada a objetos de lidar com caminhos de arquivos no python, ele garante que o meu código funcione tanto no windows quanto em outros sistemas que utilizam barra invertida
 import pymupdf #Importa a biblioteca pymupdf
 
-def read_pdf(pdf_path):
-    document = pymupdf.open(pdf_path)
+# função de extração. Ela recebe o caminho exato de onde o pdf está no pc
+def read_pdf(pdf_path): 
+    document = pymupdf.open(pdf_path) # Carregar arquivo binario do pdf para a memoria ram e criar um objeto iterável
 
-    pages = []
+    pages = [] # Aqui eu guardo as páginas extraídas
 
-    for page_number, page in enumerate(document, start=1):
-        text = page.get_text()
-
+    for page_number, page in enumerate(document, start=1): # "document" funciona como uma lista de páginas, "enumerate" é uma função nativa do python excelente para loops, retorna "page_number" = índice e o conteúdo = "page". coloquei o "start" para iniciar no 1 porque o índice começa em 0.
+        text = page.get_text() # extrai tudo o que for caractere legível, convertendo para str de py e preservando as \n.
+        
+        # Começo a criação de metadados. Quando eu for salvar isso no banco vetorial, a ideia é que a IA leia a chave "text". Quando for necessário mostrar as citações apra o usuário, o system vai olhar para as keys "page" e "source"
         page_data = {
             "text": text,
             "page": page_number,
             "source": Path(pdf_path).name
         }
 
-        pages.append(page_data)
+        pages.append(page_data) # add dict criado na lista "pages".
 
+# Importante frizar que sem o close o python manteria o documento "preso" na memória e quando o user tentasse deletar o arquivo o sistema negaria a ação com o argumento: "O arquivo está sendo usado ou aberto por outro programa".
     document.close()
+    return pages 
 
-    return pages
+
+project_root = Path(__file__).resolve().parent.parent # "__file__" contém o caminho do script atual. ".resolve()": Transforma caminhos relativos em absolutos. ".parent" sobre o nv da pasta e sai de pdf_reade.py para src, o segundo "parent" sobe mais um nível e sai de src e cai na pasta do projeto: meu-rag
+
+pdf_path = project_root / "data" / "documents" / "Testando_protótipo.pdf" # é só o caminho exato até o pdf de teste
 
 
-project_root = Path(__file__).resolve().parent.parent
-
-pdf_path = project_root / "data" / "documents" / "Testando_protótipo.pdf"
-
-pages = read_pdf(pdf_path)
+# Executa a função, apenas imprime tudo no console para eu testar se a extração funcionou.
+pages = read_pdf(pdf_path) 
 
 print(f"Total de páginas: {len(pages)}")
 
